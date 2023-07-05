42bb32bb92cfc9586acb623f4e3258f11a161e5a
miltenkot <miltenkot@gmail.com>
2023-07-05 17:37:29 +0200

    Add app icon

---
+++ DiningAlert/DiningAlert/DiningAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiningAlertApp.swift
+//  DiningAlert
+//
+//  Created by Bartłomiej on 05/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct DiningAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
